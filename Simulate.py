83397a17bab74a9947a0a88337dbd81310607752
barn3195 <barn3195@stthomas.edu>
2025-03-02 10:04:13 -0600

    cellsize updates dynamically based on width and height

--- Simulate.py
+++ Simulate.py
@@ -6,14 +6,6 @@ import pygame
 This will actually run a simulation
 """
 
-def initialize(width, height, cellsize, pomNumber, bushNumber, percentcarn):
-    pygame.init()
-    world = PomPomWorld(width, height, cellsize, pomNumber, bushNumber, percentcarn) 
-    screen = pygame.display.set_mode((world.width * world.cell_size, world.height * world.cell_size))
-    graphics = Visualize(world)
-    pygame.display.set_caption("PomPom Evolution")
-    return world, screen, graphics  # Return both world and screen
-
 def main():
     pygame.init()
 
@@ -24,6 +16,13 @@ def main():
     bushNumber = 300
     percentcarn = 0.1
     gameSpeed = 20
+
+    SCREEN_WIDTH = 700  #fixed screen width
+    SCREEN_HEIGHT = 700  #fixed screen height
+
+    cellsize = min(SCREEN_WIDTH // width, SCREEN_HEIGHT // height)
+    print(cellsize*width)
+
 
     world, screen, graphics = initialize(width, height, cellsize, pomNumber, 
                                bushNumber, percentcarn)  # Use the corrected function
@@ -45,5 +44,18 @@ def main():
 
     pygame.quit()
 
+
+
+def initialize(width, height, cellsize, pomNumber, bushNumber, percentcarn):
+    pygame.init()
+    world = PomPomWorld(width, height, cellsize, pomNumber, bushNumber, percentcarn) 
+    screen = pygame.display.set_mode((world.width * world.cell_size, world.height * world.cell_size))
+    graphics = Visualize(world)
+    pygame.display.set_caption("PomPom Evolution")
+    return world, screen, graphics  # Return both world and screen
+
+
+
+#-----------------------------------
 if __name__ == "__main__":
     main()
